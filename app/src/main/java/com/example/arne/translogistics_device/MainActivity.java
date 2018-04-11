029630e2eb81bb72df2557f87fab40a4bbb48054
T. Andersen <Tzarreth@users.noreply.github.com>
2018-04-11 10:08:10 +0200

    QR SCanner
    QR Scanner implemented

--- app/src/main/java/com/example/arne/translogistics_device/MainActivity.java
+++ app/src/main/java/com/example/arne/translogistics_device/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.arne.translogistics_device;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import static android.Manifest.permission.CAMERA;
+
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/example/arne/translogistics_device/ScanPackageActivity.java
+++ app/src/main/java/com/example/arne/translogistics_device/ScanPackageActivity.java
@@ -1,13 +1,104 @@
 package com.example.arne.translogistics_device;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
-public class ScanPackageActivity extends AppCompatActivity {
+import com.example.arne.translogistics_device.DAL.AppDataBase;
+import com.example.arne.translogistics_device.Model.Package;
+import com.google.gson.Gson;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import static android.Manifest.permission.CAMERA;
+
+public class ScanPackageActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_package);
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(getApplicationContext(), "Permission already granted", Toast.LENGTH_LONG).show();
+
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    private boolean checkPermission() {
+        return ( ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA ) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                if(mScannerView == null) {
+                    mScannerView = new ZXingScannerView(this);
+                    setContentView(mScannerView);
+                }
+                mScannerView.setResultHandler((ZXingScannerView.ResultHandler) this);
+                mScannerView.startCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+
+        Gson gson = new Gson();
+        Package p1 = convertToJson(rawResult.toString());
+
+        mScannerView.stopCamera();
+
+        AppDataBase dataBase = AppDataBase.getInstance(getApplicationContext());
+        int id = (int)dataBase.packageModel().insertPackage(p1);
+
+        Intent intent = new Intent(this, RecordDataActivity.class);
+        intent.putExtra("id", id);
+        startActivity(intent);
+
+    }
+
+    public Package convertToJson(String convertThis){
+
+        Gson gson = new Gson();
+        String json = convertThis;
+        Package p1 = gson.fromJson(json, Package.class);
+
+        return p1;
     }
 }
